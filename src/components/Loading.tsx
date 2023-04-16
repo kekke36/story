ea4584df2d5c624bf6427c6d2d38c523346d8a26
kekke36 <64622253+kekke36@users.noreply.github.com>
2023-04-16 08:00:47 +0000

    feat: loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,13 @@ import { CircularProgress } from '@mui/material'
 import React from 'react'
 
 const Loading = () => {
-  return <CircularProgress />
+  return (
+    <div className='relative'>
+      <div className='absolute inset-0 z-10 flex h-screen items-center justify-center bg-black opacity-20'>
+        <CircularProgress />
+      </div>
+    </div>
+  )
 }
 
 export default Loading
